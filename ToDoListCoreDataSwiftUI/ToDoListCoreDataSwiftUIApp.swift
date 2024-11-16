f587f8fe652658e86b30397d1b1bf9139bc41c98
Anna Panova <annapanovadev@gmail.com>
2024-11-16 10:41:07 +0100

    Initial Commit

---
+++ ToDoListCoreDataSwiftUI/ToDoListCoreDataSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListCoreDataSwiftUIApp.swift
+//  ToDoListCoreDataSwiftUI
+//
+//  Created by Anna Panova on 16.11.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListCoreDataSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
