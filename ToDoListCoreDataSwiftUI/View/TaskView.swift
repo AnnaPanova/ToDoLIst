a9cc43ed428dbc954aa456b14ef93f9a0e1a3f54
Anna Panova <annapanovadev@gmail.com>
2024-11-29 13:50:41 +0100

    Added logic to save new Task from TaskView

--- ToDoListCoreDataSwiftUI/View/TaskView.swift
+++ ToDoListCoreDataSwiftUI/View/TaskView.swift
@@ -13,15 +13,25 @@ struct TaskView: View {
     
     @StateObject var viewModel = CoreDataViewMOdel()
     @State var taskText: String = ""
+    @State var textColorVasChanged: String = "black"
     
     
     var body: some View {
         NavigationView {
             VStack{
                 TextEditor(text: $taskText)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.yellow.opacity(0.8))
+                    .foregroundColor(Color.colorFromTag(tag: textColorVasChanged))
+                    .padding()
                 HStack{
                     Button("A") {
                         //action for changing colorTag to Black
+                        // change foreground color into a TextEditor
+                    textColorVasChanged = "black"
+                       
+                        // change tagForColor inside of entity
+                        
                     }
                     .frame(width: 40, height: 30)
                     .background(Color.gray.opacity(0.3))
@@ -31,6 +41,7 @@ struct TaskView: View {
                     
                     Button("A") {
                         //action for changing colorTag to Red
+                        textColorVasChanged = "red"
                     }
                     .frame(width: 40, height: 30)
                     .background(Color.gray.opacity(0.3))
@@ -39,6 +50,7 @@ struct TaskView: View {
                   //  .padding()
                     Button("A") {
                         //action for changing colorTag to Green
+                        textColorVasChanged = "green"
                     }
                     .frame(width: 40, height: 30)
                     .background(Color.gray.opacity(0.3))
@@ -47,6 +59,7 @@ struct TaskView: View {
                   //  .padding()
                     Button("A") {
                         //action for changing colorTag to Blue
+                        textColorVasChanged = "blue"
                     }
                     .frame(width: 40, height: 30)
                     .background(Color.gray.opacity(0.3))
@@ -56,6 +69,14 @@ struct TaskView: View {
                     
                     Button("Save") {
                         //action for saving data
+                        guard taskText.count >= 1 else {return}
+                        let newTask = TaskEntity(context: viewModel.conteiner.viewContext)
+                        newTask.textOfTask = taskText
+                        newTask.colorTag = textColorVasChanged
+                        print("Trying to save")
+                        viewModel.saveChanges()
+                        print(viewModel.$fetchedEntities.first())
+                        taskText = newTask.textOfTask ?? "bla-bla"
                     }
                     .frame(width: 120, height: 50)
                     .background(Color.gray.opacity(0.5))
