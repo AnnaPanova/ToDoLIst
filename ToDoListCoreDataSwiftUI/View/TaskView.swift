ef42c351f481cb204970c791df7db82099381771
Anna Panova <annapanovadev@gmail.com>
2024-12-10 10:24:47 +0100

    Refactored Button for changing task color

--- ToDoListCoreDataSwiftUI/View/TaskView.swift
+++ ToDoListCoreDataSwiftUI/View/TaskView.swift
@@ -12,23 +12,22 @@ import CoreData
 struct TaskView: View {
     
     @StateObject var viewModel = CoreDataViewMOdel()
-    @State var textColorVasChanged: String
-    
+
     @State var selectedTask: TaskEntity?
     
     @State var taskText: String
     @State var taskColor: String
+    
+    let colorsArray: [String] = ["black", "red", "green", "blue"]
         
     init(passedTask: TaskEntity?) {
         if let task = passedTask {
             _selectedTask = State(initialValue: task)
             _taskText = State(initialValue: task.textOfTask ?? "No text")
             _taskColor = State(initialValue: task.colorTag ?? "black")
-            _textColorVasChanged = State(initialValue: task.colorTag ?? "black")
         } else {
             _taskText = State(initialValue: "")
             _taskColor = State(initialValue: "black")
-            _textColorVasChanged = State(initialValue: "black")
         }
     }
 
@@ -40,18 +39,15 @@ struct TaskView: View {
         if selectedTask != nil {
             selectedTask?.textOfTask = taskText
             selectedTask?.colorTag = taskColor
-            print("Trying to save CHANGES task")
             viewModel.saveChanges()
             viewModel.fetchTaskEntities()
-            print("CHANGED TASK: text: \(selectedTask!.textOfTask ?? "no tesk text"), color: \(selectedTask!.colorTag ?? "no color Tag ")")
         } else {
             let newTask = TaskEntity(context: viewModel.conteiner.viewContext)
             newTask.textOfTask = taskText
-            newTask.colorTag = textColorVasChanged
-            print("Trying to save NEW task")
+            newTask.colorTag = taskColor
+            selectedTask = newTask
             viewModel.saveChanges()
             viewModel.fetchTaskEntities()
-            print("NEW TASK: text: \(newTask.textOfTask ?? "no tesk text"), color: \(newTask.colorTag ?? "no color Tag ")")
         }
       
     }
@@ -63,34 +59,15 @@ struct TaskView: View {
                 TextEditor(text: $taskText)
                     .frame(maxWidth: .infinity)
                     .background(Color.yellow.opacity(0.8))
-                
                     .foregroundColor(Color.colorFromTag(tag: taskColor))
                     .padding()
                 
                 HStack{
-                    
-                    // button for changing colorTag to black
-                    ButtonForChangingColor(buttonColor: "black") {
-                       textColorVasChanged = "black"
-                        taskColor = "black"
-                    }
-                    
-                    // button for changing colorTag to red
-                    ButtonForChangingColor(buttonColor: "red") {
-                       textColorVasChanged = "red"
-                        taskColor = "red"
-                    }
-                    
-                    // button for changing colorTag to green
-                    ButtonForChangingColor(buttonColor: "green") {
-                       textColorVasChanged = "green"
-                        taskColor = "green"
-                    }
-
-                    // button for changing colorTag to blue
-                    ButtonForChangingColor(buttonColor: "blue") {
-                       textColorVasChanged = "blue"
-                        taskColor = "blue"
+                    // buttons for changing colorTag to black/red/green/blue
+                    ForEach(colorsArray, id: \.self) { color in
+                        ButtonForChangingColor(buttonColor: color) {
+                            taskColor = color
+                        }
                     }
                   
                     // button for saving task
